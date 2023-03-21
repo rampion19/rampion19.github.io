b20d810f87aad5dc02d23e0d902b5a199ac925e6
rampion19 <princegupta.pg19@gmail.com>
2023-03-21 20:11:26 +0530

    sent HTTP request and handlling the error

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
-import { useSelector } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './Store/ui-slice';
+
+
+let initial = true;
 
 function App() {
 
   const showCart = useSelector(state => state.ui.showCart)
+  const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+
+    const sendCartData = async () => {
+      dispatch(uiActions.setNotification({
+        status: "pending",
+        title: "sending...",
+        message: "Sending cart item!"
+      }))
+
+      const res = await fetch("https://cart-redux-6cac5-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart),
+        }
+      )
+      if (res.ok) {
+        dispatch(uiActions.setNotification({
+          status: "success",
+          title: "success...",
+          message: "Sent cart item successfully!"
+        }))
+      } else {
+        throw new Error("failed")
+      }
+    }
+    if(initial){
+      initial = false;
+      return;
+    }
+    sendCartData().catch(error => {
+      dispatch(uiActions.setNotification({
+        status: "error",
+        title: "Error...",
+        message: "Sent cart item failed!"
+      }))
+    })
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message} />}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+
   );
 }
 
